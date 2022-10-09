f4bd9af4ab74b60cc29d6dc3be540e44b5c29151
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2022-10-10 00:32:55 +0530

    Time: 39 ms (95.85%), Space: 36.7 MB (89.43%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,15 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//O(n) time and O(logn) space solution
+
+//BST Iterator.. two pointer
+class BSTIterator{ //has two forms of next.. one as next other as prev
+    stack<TreeNode*> call;
+    TreeNode* itr;
+    bool fwd;
+public:
+    // unlike the original BSTIterator qsn, we only need to start from the root
+    BSTIterator(TreeNode* root, bool fwd){itr = root; this->fwd = fwd; }
+    int next(){
+        while(itr || !call.empty()){
+            if(itr){
+                call.push(itr);
+                itr = fwd ? itr->left : itr->right;
+            }else{
+                itr = call.top();
+                call.pop();
+                int ret = itr->val;
+                itr = fwd ? itr->right : itr->left;
+                return ret;
+            }
+        }
+        return -1; //escape 
+    }
+};
 class Solution {
 public:
-    unordered_set<int> nums;
     bool findTarget(TreeNode* root, int k) {
         if(!root)
             return false;
-        if(nums.count(k-root->val))
-            return true;
-        nums.insert(root->val);
-        return findTarget(root->left, k) || findTarget(root->right, k);
+        BSTIterator l(root, true), r(root, false);
+        for(int i = l.next(), j = r.next(); i < j;){
+            int sum = i + j;
+            if(sum == k)
+                return true;
+            else if(sum < k)
+                i = l.next();
+            else
+                j = r.next();
+        }
+        return false;
     }
 };
