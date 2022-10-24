9465af43d8e5419a52bddca09ef686131f3d7238
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2022-10-25 00:09:24 +0530

    Time: 8 ms (90.10%), Space: 9.7 MB (74.27%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,39 +1,27 @@
 class Solution {
-    vector<map<int, int>> memo;
 public:
-    int dp(vector<int> &bits, vector<string> &arr, int i, int lets){
-        int n = bits.size();
-        if(i >= n)
-            return 0;
-        if(memo[i].count(lets))
-            return memo[i][lets];
-        int curr = bits[i];
-        if(curr == (1<<26) || (curr & lets) != 0){
-            return memo[i][lets] = dp(bits, arr, i+1, lets);
-        }else{
-            int orred = lets | curr;
-            int opt1 = arr[i].size() + dp(bits, arr, i+1, orred);
-            int opt2 = dp(bits, arr, i+1, lets);
-            return memo[i][lets] = max(opt1, opt2);
-        }
-    }
     int maxLength(vector<string>& arr) {
-        int n = arr.size();
-        memo = vector<map<int, int>>(n+1);
-        vector<int> bits(n);
-        int maxx = (1<<26);
-        for(int i = 0; i < arr.size(); i++){
-            int num = 0;
-            for(int j = 0; j < arr[i].size(); j++){
-                if(num & (1<<(arr[i][j]-'a'))){
-                    num = maxx;
-                    break;
-                }else{
-                    num |= 1<<(arr[i][j] - 'a');
-                }
+       vector<bitset<26>> m; 
+        int res=0;
+        
+        for(auto& s : arr)
+        {
+            bitset<26> bits;
+            for(char c:s) bits.set(c-'a');
+            int n = bits.count();
+            if(n != s.size()) continue;
+            
+            for(int i=m.size()-1; i>=0; i--) // avoid changes of m on the fly
+            {
+                auto& b = m[i];
+                if((b & bits).any())
+                    continue;
+                res = max<int>(res, b.count() + n);
+                m.push_back(b | bits);
             }
-            bits[i] = num;
+            m.push_back(bits);
+            res=max<int>(res, n);
         }
-        return dp(bits, arr, 0, 0);
+        return res; 
     }
 };
