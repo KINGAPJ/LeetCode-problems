6d7268f00ab7695b917d77015ee38971858cc0b4
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2022-11-20 16:14:10 +0530

    Time: 12 ms (80.96%), Space: 8.1 MB (43.51%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int res = 0;
+        int sign = 1;
+        int j = 0;
+        int n = s.size();
+        stack<int> st;
+        while(j < n){ 
+            if(s[j] == '+')
+                sign = 1;
+            else if(s[j] == '-')
+                sign = -1;
+            else if(isdigit(s[j])){
+                int num = s[j] - '0';
+                while(j + 1 < n && isdigit(s[j+1]))
+                    num = num*10 + (s[j++ +1] - '0');
+                res += num * sign;
+            }else if(s[j] == '('){
+                st.push(res);
+                st.push(sign);
+                res = 0;
+                sign = 1;
+            }else if(s[j] == ')'){
+                int xsign = st.top();
+                st.pop();
+                int xres = st.top();
+                st.pop();
+                res = res * xsign + xres;
+            }
+            j++;
+        }
+        return res;
+    }
+};
