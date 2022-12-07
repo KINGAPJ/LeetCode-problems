6b50dda32e92f630929126b7afe68b3032cdeeef
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2022-12-07 18:58:40 +0530

    Time: 362 ms (5.10%), Space: 64.5 MB (92.64%) - LeetHub

---
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root){
+            // if((low < root->val) || (high >= root->val)){
+            //     sum += root->val;
+            //     rangeSumBST(root->left, low, high);
+            // }
+            // else rangeSumBST(root->right, low, high);
+            // if((low >= root->val)|| (high < root->val)){
+            //     sum += root->val;
+            //     rangeSumBST(root->right, low, high);
+            // }
+            // else rangeSumBST(root->left, low, high);
+        // }
+        //     if(low < root->val){
+        //         sum += root->val;
+        //         rangeSumBST(root->left, low, high);
+        //     }
+        //     else rangeSumBST(root->right, low, high);
+        //     if(high > root->val){
+        //         rangeSumBST(root->right, low, high);
+        //     }
+        //     else rangeSumBST(root->left, low, high);
+        // }
+            if(low < root->val) rangeSumBST(root->left, low, high);
+            if(low<=root->val && high>= root->val) sum += root->val;
+            if(high > root->val) rangeSumBST(root->right, low, high);
+        }
+        return sum;
+    }
+};
