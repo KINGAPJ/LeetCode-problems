d7b5bd88da29fd4ca72b0687102fbd92a58783bc
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2022-12-23 15:40:23 +0530

    Time: 2723 ms (5.01%), Space: 369.9 MB (12.19%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,22 +1,31 @@
 #define ll long long
 class Solution {
+    // refer the excel sheet for expl
+    map<pair<ll, ll>, ll> memo;
 public:
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        int n = nums.size();
-        ll ans = 0;
-        vector<map<ll, int>> cnt(n);
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                ll d = (ll)nums[i] - (ll)nums[j];
-                int sum = 0;
-                if (cnt[j].find(d) != cnt[j].end()) {
-                    sum = cnt[j][d];
-                }
-                cnt[i][d] += sum + 1;
-                ans += sum;
+    // ll dp(vector<ll>& nums, ll i, ll d){
+    //     if(i == 0)
+    //         return 0;
+    //     if(memo.count({i,d}))
+    //         return memo[{i,d}];
+    //     ll curr = 0;
+    //     for(ll j = 0; j < i; j++){
+    //         memo[{i, nums[i]-nums[j]}] += (1 + dp(nums, j, nums[i] - nums[j]));
+    //     }
+    //     return memo[{i,d}] = curr;
+    // }
+    ll numberOfArithmeticSlices(vector<int>& nums) {
+        ll n = nums.size();
+        // ll ans = dp(nums, n-1, 0);
+        for(ll i = 0; i < n; i++){
+            for(ll j = 0; j < i; j++){
+                ll currcd = 0LL + nums[i]-nums[j];
+                memo[{i, currcd}] += (1 + memo[{j, currcd}]);
             }
         }
-
-        return (int)ans;
+        ll ans =  0;
+        for(auto &i: memo)
+            ans += i.second;
+        return ans - n*(n-1)/2;
     }
 };
