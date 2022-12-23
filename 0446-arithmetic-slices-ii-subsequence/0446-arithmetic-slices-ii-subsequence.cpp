43611ce223c706e9fb519768515ced9fb6f2cd42
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2022-12-23 15:49:02 +0530

    Time: 2649 ms (5.42%), Space: 370 MB (12.19%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -3,20 +3,8 @@ class Solution {
     // refer the excel sheet for expl
     map<pair<ll, ll>, ll> memo;
 public:
-    // ll dp(vector<ll>& nums, ll i, ll d){
-    //     if(i == 0)
-    //         return 0;
-    //     if(memo.count({i,d}))
-    //         return memo[{i,d}];
-    //     ll curr = 0;
-    //     for(ll j = 0; j < i; j++){
-    //         memo[{i, nums[i]-nums[j]}] += (1 + dp(nums, j, nums[i] - nums[j]));
-    //     }
-    //     return memo[{i,d}] = curr;
-    // }
     ll numberOfArithmeticSlices(vector<int>& nums) {
         ll n = nums.size();
-        // ll ans = dp(nums, n-1, 0);
         for(ll i = 0; i < n; i++){
             for(ll j = 0; j < i; j++){
                 ll currcd = 0LL + nums[i]-nums[j];
