58d6da365da5602034db7fe1461f1489e0be633c
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2022-12-25 19:56:34 +0530

    Time: 4 ms (45.29%), Space: 7.3 MB (20.84%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,64 +1,28 @@
 #define ll long long
-const ll MOD = 1e9 + 7;
+const ll mod = 1e9 + 7;
 class Solution {
+    vector<vector<ll>> memo;
 public:
-    int numTilings(int N) {
-        int T_prepre = 1, T_pre = 1;
-        int T_up_pre = 0, T_down_pre = 0;
-
-        for (int n = 2; n <= N; n++) {
-            int T_cur = (int)((0L + T_prepre + T_pre + T_up_pre + T_down_pre) % MOD);
-            int T_up_cur = (T_prepre + T_down_pre) % MOD;
-            int T_down_cur = (T_prepre + T_up_pre) % MOD;
-
-            T_prepre = T_pre;
-            T_pre = T_cur;
-
-            T_up_pre = T_up_cur;
-            T_down_pre = T_down_cur;
-        }
-
-        return T_pre;
-    }
-};
-/*
-        //placing vertical block of 2;
-        ret += dp(n-1, odd);
-        //placing 2 horizontal blocks of 2;
-        ret += dp(n-2, odd);
-        //placing L of 3 in 4 ways;
-        ret += 4*(dp(n-2, !odd));
-        /*
-            placing  .. or  ..   depending one which side  
-                    ..       ..
-            
-            ret += dp(n-2, !odd);
-            
-            ll dp(ll n, bool odd){// odd is extra 0.5
-        if(n <= 0)
+    ll dp(ll n, bool odd){
+        if(n == 0)
             return 0;
         if(n <= 2)
             return n;
-        ll ret = 0;
-        if(odd == 0){
-            /*
-                placing    . and .. to stack on left
-                          ..      .
-            
-            ret += 2*dp(n-2, !odd);
-            //placing vertical block of 2;
-            ret += dp(n-1, odd);
-            //placing 2 horizontal blocks of 2;
-            ret += dp(n-2, odd);
+        if(memo[n][odd] != -1)
+            return memo[n][odd];
+        ll curr = 0;
+        if(odd == true){ // there is an extra dot
+            curr = (curr + dp(n-1, false)) % mod; //put a three dotter and remove the extra
+            curr = (curr + dp(n-1, true)) % mod; //put a two dotter and keep the extra on opposite side
         }else{
-            /*
-                placing   .  or .. to stack on left to balance
-                          ..    .
-            
-            ret += dp(n-2, !odd);
-            //placing horizontal block of 2;
-            ret += dp(n-1, odd);
+            curr = (curr + 2*dp(n-2, true)) % mod; //three dotters in 2 ways
+            curr = (curr + dp(n-1, false)) % mod; //vertical two dotter
+            curr = (curr + dp(n-2, false)) % mod; //2 horizontal two dotters
         }
-        return ret;
+        return memo[n][odd] = curr;
     }
-*/
+    int numTilings(int n) {
+        memo = vector<vector<ll>>(n+1, (vector<ll>(2, -1)));
+        return dp(n, false) % mod;
+    }
+};
