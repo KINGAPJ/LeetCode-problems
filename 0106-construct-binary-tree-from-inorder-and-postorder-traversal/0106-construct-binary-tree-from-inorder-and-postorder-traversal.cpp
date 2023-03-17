b7ad1a97dff62f1342d43c9208e430bc19ea5b26
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-03-17 11:22:34 +0530

    Time: 16 ms (80.35%), Space: 26.5 MB (25.73%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    unordered_map<int, int> findinorderind;
     TreeNode* build(vector<int>& in, vector<int>& po, int in_l, int in_r, int po_l, int po_r){
         if(in_l < 0 || in_r < 0 || po_l < 0 || po_r < 0 || in_l > in_r || po_l > po_r)
             return nullptr;
@@ -18,20 +19,14 @@ public:
             return nullptr;
         TreeNode* root = new TreeNode(po[po_r]);
         int rootval = po[po_r];
-        int inorderroot = -1;
-        int postorderright = -1;
-        for(int i = in_l; i <= in_r; i++){
-            if(in[i] == rootval){
-                inorderroot = i;
-                break;
-            }
-        }
+        int inorderroot = findinorderind[rootval];
         root->left = build(in, po, in_l, inorderroot-1, po_l, po_l + (inorderroot-in_l-1));
         root->right = build(in, po, inorderroot+1, in_r, po_l + (inorderroot-in_l), po_r-1);
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        cout<<'\n';
+        for(int i = 0; i < inorder.size(); i++)
+            findinorderind[inorder[i]] = i;
         return build(inorder, postorder, 0, inorder.size()-1, 0, postorder.size()-1);
     }
 };
