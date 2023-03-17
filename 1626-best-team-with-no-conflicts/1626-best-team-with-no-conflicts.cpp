9f390b24dc7f3c742002a6b3031550815707a745
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-03-17 23:32:28 +0530

    Time: 867 ms (5.00%), Space: 135.5 MB (6.27%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,36 +1,66 @@
-class Solution {
+class Solution{
+    vector<vector<int>> memo;
 public:
-    int findMaxScore(vector<vector<int>>& dp, vector<pair<int, int>>& ageScorePair, int prev, int index) {
-        // Return 0 if we have iterated over all the players.
-        if (index >= ageScorePair.size()) {
+    int dp(vector<vector<int>>& ageScore, int i, int prevSelected){
+        int n = ageScore.size();
+        if(i == n)
             return 0;
+        if(memo[i][prevSelected] != -1)
+            return memo[i][prevSelected];
+        int ans = 0;
+        if(prevSelected == n || 
+           !(ageScore[prevSelected][0] < ageScore[i][0] &&
+            ageScore[prevSelected][1] > ageScore[i][1])
+          ){
+            ans += ageScore[i][1] + dp(ageScore, i+1, i);
         }
-        
-        // We have already calculated the answer, so no need to go into recursion.
-        if (dp[prev + 1][index] != -1) {
-            return dp[prev + 1][index];
-        }
-
-        // If we can add this player, return the maximum of two choices we have.
-        if (prev == -1 || ageScorePair[index].second >= ageScorePair[prev].second) {
-            return dp[prev + 1][index] = max(findMaxScore(dp, ageScorePair, prev, index + 1), 
-                       ageScorePair[index].second + findMaxScore(dp, ageScorePair, index, index + 1));
-        }
-        
-        // This player cannot be added; return the corresponding score.
-        return dp[prev + 1][index] = findMaxScore(dp, ageScorePair, prev, index + 1);
+        ans = max(ans, dp(ageScore, i+1, prevSelected));
+        return memo[i][prevSelected] = ans;
     }
-    
-    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        vector<pair<int, int>> ageScorePair;
-        for (int i = 0; i < scores.size(); i++) {
-            ageScorePair.push_back({ages[i], scores[i]});
-        }
-        
-        // Sort in ascending order of age and then by score.
-        sort(ageScorePair.begin(), ageScorePair.end());
-        // Mark all the states as -1, denoting not yet calculated.
-        vector<vector<int>> dp(scores.size(), vector<int>(scores.size(), -1));
-        return findMaxScore(dp, ageScorePair, -1, 0);
+    int bestTeamScore(vector<int>& scores, vector<int>& ages){
+        int n = scores.size();
+        vector<vector<int>> ageScore(n);
+        memo = vector<vector<int>>(n, vector<int>(n+1, -1));
+        for(int i = 0; i < n; i++)
+            ageScore[i] = {ages[i], scores[i]};
+        sort(ageScore.begin(), ageScore.end());
+        return dp(ageScore, 0, n);
     }
 };
+// class Solution {
+//     vector<int> memo;
+// public:
+//     int dp(vector<vector<int>>& ageScore, int i, vector<int>
+//            // vector<vector<int>>& formedList){
+//         int n = ageScore.size();
+//         if(i == n)
+//             return 0;
+//         if(memo[i] != -1)
+//             return memo[i];
+//         int highestLowerAgeScore = 0, ans = 0;
+//         // for(int j = 0; j < formedList.size() && formedList[j][0] < ageScore[i][0]; j++){
+//         //     highestLowerAgeScore = max(highestLowerAgeScore, formedList[j][1]);
+//         // }
+//         cout<<i<<" "<<highestLowerAgeScore<<"\n";
+//         for(auto &j: formedList)
+//             cout<<j[0]<<':'<<j[1]<<' ';
+//         cout<<'\n';
+//         if(ageScore[i][1] >= highestLowerAgeScore){
+//             formedList.push_back(ageScore[i]);
+//             ans = ageScore[i][1] + dp(ageScore, i+1, formedList);
+//             formedList.pop_back();
+//         }
+//         ans = max(ans, dp(ageScore, i+1, formedList));
+//         return memo[i] = ans;
+//     }
+//     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+//         int n = scores.size();
+//         vector<vector<int>> ageScore(n), formedList;
+//         for(int i = 0; i < n; i++){
+//             ageScore[i] = {ages[i], scores[i]};
+//         }
+//         sort(ageScore.begin(), ageScore.end());
+//         memo = vector<int>(n, -1);
+//         return dp(ageScore, 0, formedList);
+//     }
+// };
