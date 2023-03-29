f149d32237745b4d64fcf81939e3d29c7dd660dc
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-03-29 13:16:19 +0530

    Time: 4 ms (88.09%), Space: 8.1 MB (89.59%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -6,23 +6,16 @@ public:
             return 0;
         int negpref = 0, pospref = 0;
         int posptr = lower_bound(sat.begin(), sat.end(), 0) - sat.begin();
-        // if(posptr == 0)
-        //     return accumulate(sat.begin(), sat.end(), 0);
         int negptr = posptr - 1;
         int ans = 0, n = sat.size(), pref = 0;
-        // for(int i = 0; i < n; i++)
-        //     cout<<sat[i]<<' ';
-        // cout<<'\n';
         for(int i = posptr; i < n; i++){
             ans += (i-posptr+1) * sat[i];
             pref += sat[i];
         }
-        // cout<<ans<<' '<<pref<<'\n';
         for(int i = negptr; i >= 0; i--){
             if(pref > -sat[i]){
                 ans += pref + sat[i];
                 pref += sat[i];
-                // cout<<ans<<' '<<pref<<'\n';
             }
         }
         return ans;
