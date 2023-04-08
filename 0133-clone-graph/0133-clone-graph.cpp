8b5d2a0613b75aefb5638d86eee5eb0273857357
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-04-08 15:11:10 +0530

    Time: 7 ms (61.34%), Space: 8.7 MB (65.38%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -37,11 +37,34 @@ public:
         }
         node->neighbors = currNbr;
     }
+    void bfs(Node* node){
+        queue<Node*> q;
+        // vis.insert({node->val, node});
+        q.push(node);
+        while(q.size()){
+            auto u = q.front();
+            q.pop();
+            vis.insert({u->val, u});
+            vector<Node*> currNbr;
+            for(auto &i: u->neighbors){
+                Node* nbr = NULL;
+                if(!vis.count(i->val)){
+                    nbr = new Node(i->val, i->neighbors);
+                    vis.insert({i->val, nbr});
+                    q.push(nbr);
+                }else
+                    nbr = vis[i->val];
+                currNbr.push_back(nbr);
+            }
+            u->neighbors = currNbr;
+        }
+    }
     Node* cloneGraph(Node* node) {
         if(!node)
             return NULL;
         root = new Node(node->val, node->neighbors);
-        dfs(root);
+        // dfs(root);
+        bfs(root);
         return root;
     }
 };
