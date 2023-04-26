f270464945d17195d80ebcbe9e9782926d402670
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-04-26 17:16:05 +0530

    Time: 398 ms (34.82%), Space: 44.1 MB (5.01%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,19 +1,35 @@
 class Solution {
+    vector<vector<long>> memo;
+    vector<vector<int>> piles;
 public:
+    long dp(int i, int k){
+        // cout<<"i:"<<i<<" k:"<<k<<'\n';
+        if(k == 0)
+            return 0;
+        if(i == 0)
+            return piles[i].size() <= k ? piles[i].back() : piles[i][k];
+        if(memo[i][k] != -1)
+            return memo[i][k];
+        long curr = 0;
+        for(int j = 0; j <= min(k, (int)piles[i].size()-1); j++){
+            // if(j < piles[i].size()){
+                curr = max(curr, dp(i-1, k-j) + piles[i][j]);
+            // }else{
+            //     curr = max(curr, dp(i-1, k));
+            // }
+        }
+        return memo[i][k] = curr;
+    }
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         int n = piles.size();
-        vector dp(n + 1, vector<int>(k + 1));
-        for (int i = 1; i <= n; i++) {
-            for (int coins = 0; coins <= k; coins++) {
-                int currentSum = 0;
-                for (int currentCoins = 0; currentCoins <= min((int)piles[i - 1].size(), coins); currentCoins++) {
-                    if (currentCoins > 0) {
-                        currentSum += piles[i - 1][currentCoins - 1];
-                    }
-                    dp[i][coins] = max(dp[i][coins], dp[i - 1][coins - currentCoins] + currentSum);
-                }
-            }
+        memo = vector<vector<long>> (n+1, vector<long>(k+1, -1));
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j < piles[i].size(); j++)
+                piles[i][j] += piles[i][j-1];
+            piles[i].insert(piles[i].begin(), 0);
         }
-        return dp[n][k];
+        this->piles = piles;
+        // cout<<'\n';
+        return dp(n-1, k);
     }
 };
