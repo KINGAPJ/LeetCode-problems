e5c7942fb53bf1949bb7a2c6deff91429d21a404
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-05-08 11:42:03 +0530

    Time: 386 ms (25.34%), Space: 118.8 MB (58.90%) - LeetHub

--- 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -1,42 +1,14 @@
-class BIT{
-    vector<int> bit;
-public:
-    BIT(int n){
-        bit = vector<int> (n+2);
-    }
-    void add(int i){
-        while(i < bit.size()){
-            bit[i] += 1;
-            i += i & (-i);
-        }
-    }
-    int query(int i){
-        int sum = 0;
-        while(i > 0){
-            sum += bit[i];
-            i -= i & (-i);
-        }
-        return sum;
-    }
-};
-
 class Solution {
 public:
-    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
-        int n = obstacles.size();
-        
-        // lis[i] records the lowest increasing sequence of length i + 1.
-        vector<int> answer(n, 1), lis;
-        
-        for (int i = 0; i < n; ++i) {
-            // Find the rightmost insertion position idx.
-            int idx = upper_bound(lis.begin(), lis.end(), obstacles[i]) - lis.begin();
-            if (idx == lis.size())
-                lis.push_back(obstacles[i]);
-            else
-                lis[idx] = obstacles[i];
-            answer[i] = idx + 1;
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obs) {
+        int n = obs.size();
+        vector<int> vals(n, INT_MAX);
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++){
+            int ind = upper_bound(vals.begin(), vals.end(), obs[i]) - vals.begin();
+            vals[ind] = obs[i];
+            ans[i] = ind + 1;
         }
-        return answer;
+        return ans;
     }
 };
