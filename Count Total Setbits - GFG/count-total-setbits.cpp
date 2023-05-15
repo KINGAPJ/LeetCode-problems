3b6386c06d43e07f9eb87399e1d7c13a5a93d093
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-05-15 23:38:35 +0530

    Added solution - LeetHub

--- Count Total Setbits - GFG/count-total-setbits.cpp
+++ Count Total Setbits - GFG/count-total-setbits.cpp
@@ -10,24 +10,19 @@ class Solution {
   public:
     ll countBits(long long N) {
         // code here
-        int logg = ceil(log2(N))+1;
-        vector<ll> dp(ceil(log2(N))+1, 0);
-        vector<ll> pref(ceil(log2(N))+1, 0);
+        ll logg = ceil(log2(N))+1;
+        vector<ll> dp(logg, 0);
+        vector<ll> pref(logg, 0);
         dp[1] = 1, pref[1] = 1;
         for(int i = 2; i < logg; i++){
             dp[i] = pref[i-1] + (1LL<<(i-1));
             pref[i] = dp[i] + pref[i-1];
         }
-        // for(int i = 0; i < logg; i++){
-        //     cout<<dp[i]<<' '<<pref[i]<<'\n';
-        // }
         ll ans = 0, cnt = 0;
         for(ll i = logg; i >= 0; i--){
             if(N & (1LL<<i)){
-                ans += pref[i]+1;
-                // cout<<pref[i]<<'\n';
+                ans += (pref[i]+1);
                 ans += (1LL<<i) * cnt++;
-                // cout<<ans<<'\n';
             }
         }
         return ans;
