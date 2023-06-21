88b7c409119d482547f24ce26aee00465f1412db
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-06-21 12:17:30 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (89.20%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -0,0 +1,53 @@
+class Solution {
+    vector<string> ans;
+    string s;
+    void f(int i, int lefts, string &curr, int rems){
+        int n = s.size();
+        if(rems < 0)
+            return;
+        if(i == n){
+            if(lefts == 0){
+                ans.push_back(curr);
+            }
+            return;
+        }
+        int j = i;
+        while(j < n && s[j] == s[i])
+            j++;
+        j--;
+        if(s[i] == ')'){
+            if(lefts > 0){
+                curr.push_back(s[i]);
+                f(i+1, lefts-1, curr, rems);
+                curr.pop_back();
+            }
+            f(j+1, lefts, curr, rems-(j-i+1));
+        }else if(s[i] == '('){
+            curr.push_back(s[i]);
+            f(i+1, lefts+1, curr, rems);
+            curr.pop_back();
+            f(j+1, lefts, curr, rems-(j-i+1));
+        }else{
+            curr.push_back(s[i]);
+            f(i+1, lefts, curr, rems);
+            curr.pop_back();
+        }
+    }
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        this->s = s;
+        int rems = 0, lefts = 0;
+        for(int i = 0; i < s.size(); i++){
+            lefts += (s[i] == '(');
+            if(s[i] == ')'){
+                if(lefts == 0)
+                    rems++;
+                else lefts--;
+            }
+        }
+        rems += lefts;
+        string curr = "";
+        f(0, 0, curr, rems);
+        return ans;
+    }
+};
