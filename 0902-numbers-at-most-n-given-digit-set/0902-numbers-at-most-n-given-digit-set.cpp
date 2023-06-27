de6ec6897fe89b29fe7e2acaf49b11a2ce0dc0d8
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-06-27 19:36:48 +0530

    Time: 3 ms (46.30%), Space: 8.3 MB (9.59%) - LeetHub

--- 0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.cpp
+++ 0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.cpp
@@ -1,42 +1,39 @@
 class Solution {
-    vector<int> num;
-    vector<string> digs;
+    vector<int> number;
+    vector<string> digits;
     vector<vector<vector<int>>> dp;
     int f(int i, bool tight, bool ldg){
-        int n = num.size();
+        int n = number.size();
         if(i == n)
             return 1;
         if(dp[i][tight][ldg] != -1)
             return dp[i][tight][ldg];
         int ans = 0;
         bool newtight = false;
-        int k = tight ? num[i] : 9;
-        // cout<<k<<'\n';
-        if(ldg){
+        if(ldg)
             ans += f(i+1, false, ldg);
-        }
-        for(int j = 0; j < digs.size() && digs[j][0]-'0' <= k; j++){
-            // cout<<i<<(tight?" true ":" false ")<<digs[j][0]-'0'<<'\n';
-            newtight = digs[j][0]-'0' == num[i] ? tight : false;
+        int k = tight ? number[i] : 9;
+        for(int j = 0; j < digits.size() && digits[j][0]-'0' <= k; j++){
+            newtight = digits[j][0]-'0' == number[i] ? tight : false;
             ans += f(i+1, newtight, false);
         }
         return dp[i][tight][ldg] = ans;
     }
-    void digits(int n){
+    void getDigits(int n){
         vector<int> dig;
         while(n){
             dig.push_back(n%10);
             n /= 10;
         }
         reverse(dig.begin(), dig.end());
-        num = dig;
+        number = dig;
     }
 public:
-    int atMostNGivenDigitSet(vector<string>& digts, int n) {
-        digits(n);
-        sort(digts.begin(), digts.end());
-        digs = digts;
-        int x = num.size();
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        getDigits(n);
+        sort(digits.begin(), digits.end());
+        this->digits = digits;
+        int x = number.size();
         dp = vector(x+1, vector(2, vector<int>(2, -1)));
         return f(0, true, true)-1;
     }
