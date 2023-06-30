456f02837d69450858bcdaaafe8de6a1f126c210
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-06-30 13:16:01 +0530

    Time: 435 ms (72.07%), Space: 116.1 MB (64.86%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -1,45 +1,73 @@
+class DSU{
+    vector<vector<pair<int, int>>> par;
+    vector<vector<int>> sz;
+public:
+    DSU(int row, int col){
+        par = vector(row, vector<pair<int, int>>(col));
+        sz = vector(row, vector<int>(col, 1));
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                par[i][j] = {i, j};
+                sz[i][j] = 1;
+            }
+        }
+    }
+    pair<int, int> find(pair<int, int> x){
+        if(par[x.first][x.second] == x)
+            return x;
+        return par[x.first][x.second] = find(par[x.first][x.second]);
+    }
+    void join(pair<int, int> x, pair<int, int> y){
+        auto a = find(x);
+        auto b = find(y);
+        if(a != b){
+            if(sz[a.first][a.second] < sz[b.first][b.second]){
+                swap(a, b);
+            }
+            par[b.first][b.second] = a;
+            sz[a.first][a.second] += sz[b.first][b.second];
+        }
+    }
+};
 class Solution {
     int m, n;
     bool isv(int r, int c){return r >= 0 && r < m && c >= 0 && c < n;}
-    int ds[5] = {0, -1, 0, 1, 0};
+    int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 public:
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-        m = row, n = col;
-        int lo = 1, hi = row*col;
-        int ans = 1;
-        while(lo <= hi){
-            int mid = lo + (hi-lo)/2;
-            vector grid(row, vector<int>(col, 0));
-            for(int i = 0; i < mid; i++){
-                grid[cells[i][0]-1][cells[i][1]-1] = 1;
-            }
-            queue<pair<int, int>> q;
-            for(int i = 0; i < col; i++){
-                if(grid[0][i] == 0)
-                    q.push({0, i});
-            }
-            bool yes = false;
-            while(!q.empty()){
-                auto [i, j] = q.front();
-                q.pop();
-                if(i == row-1){
-                    yes = true;
-                    break;
-                }
-                for(int k = 0; k < 4; k++){
-                    int r = i+ds[k], c = j+ds[k+1];
-                    if(isv(r, c) && grid[r][c] == 0){
-                        grid[r][c] = 1;
-                        q.push({r, c});
-                    }
-                }
-            }
-            if(yes){
-                lo = mid + 1;
-                ans = max(ans, mid);
-            }else
-                hi = mid - 1;
+        m = row+1, n = col;
+        // cout<<'\n';
+        DSU dsu(row+1, col); //+1 for dummy 0 nodes
+        vector grid(row+1, vector<int>(col));
+        for(int i = 1; i < row+1; i++){
+            dsu.join({0, 0}, {i, 0});
+            dsu.join({0, col-1}, {i, col-1});
         }
-        return ans;
+        for(int i = 0; i < cells.size(); i++){
+            grid[cells[i][0]][cells[i][1]-1] = 1;
+            // for(int p = 0; p <= row; p++){
+            //     for(int q = 0; q < col; q++){
+            //         cout<<grid[p][q]<<' ';
+            //     }
+            //     cout<<'\n';
+            // }
+            for(int k = 0; k < 8; k++){
+                int r = cells[i][0] + dx[k], c = cells[i][1]-1 + dy[k];
+                if(isv(r, c) && grid[r][c] == 1){
+                    dsu.join({cells[i][0], cells[i][1]-1}, {r, c});
+                    // cout<<cells[i][0]<<' '<<cells[i][1]<<' '<<r<<' '<<c<<":\n";
+                    // auto [g1, h1] = dsu.find({cells[i][0], cells[i][1]-1});
+                    // auto [g2, h2] = dsu.find({r, c});
+                    // cout<<g1<<' '<<g2<<' '<<h1<<' '<<h2<<'\n';
+                }
+                // auto [g1, h1] = dsu.find({0, 0});
+                // auto [g2, h2] = dsu.find({0, col-1});
+                // cout<<g1<<' '<<g2<<' '<<h1<<' '<<h2<<'\n';
+                if(dsu.find({0, 0}) == dsu.find({0, col-1}))
+                    return i;
+            }
+        }
+        return cells.size();
     }
 };
