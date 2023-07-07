24b87e9a0bdde8fb459592a5b266756764256a2e
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-07-07 18:47:57 +0530

    Time: 100 ms (10.28%), Space: 10 MB (59.24%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,51 +1,24 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        int l = 0, r = 0;
-        int n = answerKey.size();
-        int currK = 0, ans = k;
-        while(r < n && currK < k){
-            if(answerKey[r] == 'F')
-                currK++;
-            ans = max(ans, r-l+1);
-            r++;
+        int maxSize = k;
+        unordered_map<char, int> count;
+        for (int i = 0; i < k; i++) {
+            count[answerKey[i]]++;
         }
-        if(r == n)
-            return n;
-        while(r < n){
-            currK += answerKey[r] == 'F';
-            if(currK > k){
-                while(l < r && currK > k){
-                    currK -= answerKey[l] == 'F';
-                    l++;
-                }
-            }else{
-                ans = max(ans, r-l+1);
+        
+        int left = 0;
+        for (int right = k; right < answerKey.length(); right++) {
+            count[answerKey[right]]++;
+            
+            while (min(count['T'], count['F']) > k) {
+                count[answerKey[left]]--;
+                left++;
             }
-            r++;
+            
+            maxSize = max(maxSize, right - left + 1);
         }
-        // cout<<ans<<'\n';
-        l = 0, r = 0, currK = 0;
-        while(r < n && currK < k){
-            if(answerKey[r] == 'T')
-                currK++;
-            ans = max(ans, r-l+1);
-            r++;
-        }
-        if(r == n)
-            return n;
-        while(r < n){
-            currK += answerKey[r] == 'T';
-            if(currK > k){
-                while(l < r && currK > k){
-                    currK -= answerKey[l] == 'T';
-                    l++;
-                }
-            }else{
-                ans = max(ans, r-l+1);
-            }
-            r++;
-        }
-        return ans;
+                    
+        return maxSize;
     }
 };
