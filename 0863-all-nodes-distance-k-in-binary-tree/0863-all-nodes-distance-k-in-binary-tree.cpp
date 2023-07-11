dffdb470bb9d9e732455cf07e17fa6f1edf9a221
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-07-11 11:56:02 +0530

    Time: 11 ms (27.68%), Space: 16.5 MB (5.39%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    unordered_map<TreeNode*, TreeNode*> par;
+    unordered_set<TreeNode*> vis;
+    vector<int> res;
+    void set_par(TreeNode* node, TreeNode* parent){
+        if(!node)
+            return;
+        par[node] = parent;
+        set_par(node->left, node);
+        set_par(node->right, node);
+    }
+    void dfs(TreeNode* node, int k){
+        if(!node)
+            return;
+        vis.insert(node);
+        if(k == 0)
+            res.push_back(node->val);
+        if(node->left && !vis.count(node->left)){
+            dfs(node->left, k-1);
+        }
+        if(node->right && !vis.count(node->right)){
+            dfs(node->right, k-1);
+        }
+        if(par[node] && !vis.count(par[node])){
+            dfs(par[node], k-1);
+        }
+    }
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        set_par(root, nullptr);
+        dfs(target, k);
+        return res;
+    }
+};
