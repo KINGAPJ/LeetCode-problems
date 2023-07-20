7b9aa9f33ffadd920fcfe9cf9e4ce32abc2920e5
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-07-20 21:53:19 +0530

    Time: 6 ms (50.91%), Space: 6.9 MB (82.12%) - LeetHub

--- 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
+++ 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
@@ -1,71 +1,38 @@
 class Solution {
-    vector<vector<int>> dp;
-    string s;
-    bool check(string &a, string &b){
-        int n = a.size();
-        if(a.size() != b.size())
-            return false;
-        for(int i = 0; i < n; i++){
-            if(a[i] != b[n-1-i])
-                return false;
-        }
-        return true;
-    }
-    int f(int l, int r){
-        int n = s.size();
-        // cout<<l<<' '<<r<<'\n';
-        if(l > r || r >= n || l < 0)
-            return 0;
-        if(l == r)
-            return 1;
-        if(dp[l][r] != -1)
-            return dp[l][r];
-        if(s[l] == s[r])
-            return dp[l][r] = 2 + f(l+1, r-1);
-        string a = "", b = "";
-        a.push_back(s[l]);
-        b.push_back(s[r]);
-        l++, r--;
-        while(!check(a, b) && l < r){
-            // cout<<a<<' '<<b<<'\n';
-            a.push_back(s[l++]);
-            b.push_back(s[r--]);
-        }
-        // cout<<a<<' '<<b<<'\n';
-        if(check(a, b))
-            return dp[l][r] = 2 + f(l, r);
-        else
-            return dp[l][r] = 1;
-        // return dp[l][r] = 2 + f(l, r);
-    }
+    const int mod = 1e9 + 7;
 public:
     int longestDecomposition(string text) {
         int n = text.size();
-        s = text;
-        dp = vector (n, vector<int>(n, -1));
-        return f(0, n-1);
-        // for(int l = 0; l < n; l++)
-        //     for(int r = l; r < n; r++)
-        //         dp[l][r] = 1;
-        // for(int l = n-2; l >= 0; l--){
-        //     for(int r = l+1; r < n; r++){
-        //         if(text[l] == text[r])
-        //             dp[l][r] = max(dp[l][r], 2 + dp[l+1][r-1]);
-        //         else{
-        //             dp[l][r] = max(dp[l][r], min(dp[l+1][r], dp[l][r-1]));
-        //             // if(text[l] == text[r-1])
-        //             //     dp[l][r] = max(dp[l][r], dp[l+1][r]);
-        //         }
-        //     }
-        // }
-        // for(int i = 0; i < n; i++)
-        //     cout<<text[i]<<' ';
-        // cout<<'\n';
-        // for(int l = 0; l < n; l++){
-        //     for(int r = 0; r < n; r++)
-        //         cout<<dp[l][r]<<' ';
-        //     cout<<'\n';
-        // }
-        // return dp[0][n-1];
+        vector<long> powers(n+1, 1);
+        vector<long> prefhash(n);
+        long prime = 31; 
+        for(long i = 1; i <= n; i++)
+            powers[i] = (powers[i-1]*prime)%mod;
+        prefhash[0] = text[0]-'a'+1;
+        for(long i = 1; i < n; i++)
+            prefhash[i] = (prefhash[i-1] + (text[i]-'a'+1)*powers[i])%mod;
+        int maxlen = n/2, ans = 0, st = 0;
+        maxlen++;
+        for(int len = 1; len <= maxlen+1; len++){
+            int l1 = st, r1= st+len-1, l2=n-st-len, r2=l2+len-1;
+            if(l2 <= r1){
+                ans++;
+                break;
+            }
+            long compute = (prefhash[r1]- ((l1-1)>=0 ? prefhash[l1-1]: 0)+mod)%mod;
+            compute = (compute * powers[n-st-1])%mod;
+
+            long compute2 = (prefhash[r2]-((l2-1)>=0 ? prefhash[l2-1]: 0)+mod)%mod;
+            compute2 = (compute2 * powers[n-l2-1])%mod;
+
+            if(compute == compute2){
+                ans += 2;
+                st += len;
+                len = 0;
+                if(r1+1 == l2)
+                    break;
+            }
+        }
+        return ans;
     }
 };
