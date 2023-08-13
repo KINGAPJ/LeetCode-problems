57d2ec32531c43c2726974cfe0c38eb55489f066
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-08-13 15:37:19 +0530

    Time: 129 ms (88.31%), Space: 83.9 MB (92.92%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,57 +1,20 @@
 class Solution {
-    vector<int> dp, arr;
-    bool f(int i){
-        int n = arr.size();
-        if(i == n)
-            return true;
-        if(dp[i] != -1)
-            return dp[i];
-        bool ret = false;
-        //case 1
-        bool cases = false;
-        if(i+1 < n && arr[i] == arr[i+1])
-            cases = true;
-        if(cases)
-            ret |= f(i+2);
-        cases = true;
-        for(int j = i+1; j-i+1 <= 3; j++){
-            if(j >= n || arr[j] != arr[i]){
-                cases = false;
-                break;
-            }
-        }
-        if(cases)
-            ret |= f(i+3);
-        cases = true;
-        for(int j = i+1; j-i+1 <= 3; j++){
-            if(j >= n || arr[j]-arr[i] != j-i){
-                cases = false;
-                break;
-            }
-        }
-        if(cases)
-            ret |= f(i+3);
-        return dp[i] = ret;
-    }
 public:
     bool validPartition(vector<int>& nums) {
         int n = nums.size();
-        dp = vector<int>(n, -1);
-        arr = nums;
-        return f(0);
+        vector<bool> dp(3);
+        dp[0] = true;
+        for(int i = 0; i < n; i++){
+            int ind = i+1;
+            bool ans = false;
+            if(i-1 >= 0 && nums[i-1] == nums[i])
+                ans |= dp[(ind-2)%3];
+            if(i-2 >= 0 && nums[i-2] == nums[i-1] && nums[i-1] == nums[i])
+                ans |= dp[(ind-3)%3];
+            if(i-2 >= 0 && nums[i-2]+1 == nums[i-1] && nums[i-1]+1 == nums[i])
+                ans |= dp[(ind-3)%3];
+            dp[ind%3] = ans;
+        }
+        return dp[n%3];
     }
 };
-/*bool dp[3];
-        memset(dp, 0, sizeof(dp));
-        int n = nums.size();
-        if(nums[n-1] == nums[n-2])
-            dp[n-2] = true;
-        if(n == 2)
-            return dp[0];
-        if(dp[n-2] && nums[n-2] == nums[n-3] || nums[n-3]+1 == nums[n-2] && nums[n-2]+1 == nums[n-1])
-            dp[n-3] = true;
-        if(n == 3)
-            return dp[0];
-        for(int i = n-4; i >= 0; i--){
-            
-        }*/
