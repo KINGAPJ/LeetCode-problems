4d77b2e84eb17ac1d92abcbf963a4566fb54be9e
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-08-26 12:07:34 +0530

    Time: 651 ms (80.94%), Space: 34.6 MB (94.48%) - LeetHub

---
+++ 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    vector<int> one;
+    vector<int> nums;
+    int ans = INT_MAX;
+    void generate(int i, int sum){
+        int n = nums.size();
+        if(i == n/2){
+            one.push_back(sum);
+            return;
+        }
+        generate(i+1, sum+nums[i]);
+        generate(i+1, sum);
+    }
+    void check(int i, int sum, int& goal){
+        int n = nums.size();
+        if(i == n){
+            int tar = goal - sum;
+            int moreind = lower_bound(one.begin(), one.end(), tar) - one.begin();
+            int lessind = moreind-1;
+            if(moreind < one.size())
+                ans = min(ans, abs(goal-(sum+one[moreind])));
+            if(lessind >= 0)
+                ans = min(ans, abs(goal-(sum+one[lessind])));
+            return;
+        }
+        check(i+1, sum+nums[i], goal);
+        check(i+1, sum, goal);
+    }
+public:
+    int minAbsDifference(vector<int>& nums, int goal) {
+        this->nums = nums;
+        generate(0, 0);
+        int n = nums.size();
+        sort(one.begin(), one.end());
+        check(n/2, 0, goal);
+        int tar = goal;
+        int moreind = lower_bound(one.begin(), one.end(), tar) - one.begin();
+        int lessind = moreind-1;
+        if(moreind < n)
+            ans = min(ans, abs(goal-(one[moreind])));
+        if(lessind >= 0)
+            ans = min(ans, abs(goal-(one[lessind])));
+        return ans;
+    }
+};
