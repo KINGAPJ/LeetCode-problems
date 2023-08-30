f923eb65df8528ba265de577fe5b80d77be72969
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-08-30 10:36:42 +0530

    Time: 60 ms (80.56%), Space: 232.8 MB (17.35%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,54 +1,29 @@
 class Solution {
-public:
-    map<pair<int, int>, int> memo;
-    bool dp(vector<int> &stones, int i, int k){
+    vector<int> stones;
+    vector<vector<int>> dp;
+    int f(int i, int p){
         int n = stones.size();
-        if(i == n-1){
-            return true;
+        // cout<<p<<" "<<i<<endl;
+        if(i == n-1) 
+            return 1;
+        if(dp[i][p] != -1) 
+            return dp[i][p];
+        int prevjump = stones[i] - stones[p];
+        int res = 0;
+        for(int j = prevjump-1; j <= prevjump+1; j++) {
+            int nextStone = stones[i] + j;
+            int ind = lower_bound(stones.begin(), stones.end(), nextStone) - stones.begin();
+            if(ind != n && ind != i && stones[ind] == nextStone){
+                res = (res || (f(ind, i)));
+            }
         }
-        if(memo.count({i, k}))
-            return memo[{i, k}] == 1;
-        int k_minus_1 = -1;
-        if(k > 1){
-            k_minus_1 = lower_bound(stones.begin()+i+1, stones.end(), stones[i] + k-1) - stones.begin();
-            if(k_minus_1 >= n or stones[k_minus_1] != stones[i]+k-1)
-                k_minus_1 = -1;
-        }
-        int k_ = -1;
-        k_ = lower_bound(stones.begin()+i+1, stones.end(), stones[i] + k) - stones.begin();
-        if(k_ >= n or stones[k_] != stones[i]+k)
-            k_ = -1;
-        int k_plus_1 = -1;
-        k_plus_1 = lower_bound(stones.begin()+i+1, stones.end(), stones[i] + k+1) - stones.begin();
-        if(k_plus_1 >= n or stones[k_plus_1] != stones[i]+k+1)
-            k_plus_1 = -1;
-        bool yes = false;
-        if(k_minus_1 != -1){
-            yes |= dp(stones, k_minus_1, k-1);
-            if(yes)
-                return memo[{i, k}] = yes;
-        }
-        if(k_ != -1){
-            yes |= dp(stones, k_, k);
-            if(yes)
-                return memo[{i, k}] = yes;
-        }
-        if(k_plus_1 != -1){
-            yes |= dp(stones, k_plus_1, k+1);
-            if(yes)
-                return memo[{i, k}] = yes;
-        }
-        return memo[{i, k}] = yes;
+        return dp[i][p] = res;
     }
+public:    
     bool canCross(vector<int>& stones) {
-        if(stones[1] != 1)
-            return false;
-        return dp(stones, 1, 1);
+        this->stones = stones;
+        int n = stones.size();
+        dp = vector (n, vector<int>(n, -1));
+        return f(0, 0);
     }
 };
-// cout<<"{"<<i<<","<<k<<"}: "<<k_minus_1<<' '<<k_<<' '<<k_plus_1<<' '<<endl;
-// bool yes = dp(stones, 1, 1);
-//         for(auto &i: memo){
-//             cout<<"{"<<i.first.first<<","<<i.first.second<<"}: "<<i.second<<endl;
-//         }
-//         return yes;
