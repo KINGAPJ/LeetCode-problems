7458b83fe53af6c51c0916f5b30c53dea42814fe
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-10-01 18:50:41 +0530

    Time: 182 ms (5.76%), Space: 64.3 MB (6.73%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -4,23 +4,10 @@ public:
         map<int, int> mp;
         int n = nums.size();
         int minm = nums[0];
-        for(int i = 2; i < n; i++){
+        for(int i = 2; i < n; i++)
             mp[nums[i]]++;
-        }
-        // cout<<'\n';
         for(int i = 1; i < n-1; i++){
-            // for(auto &x: mp){
-            //     cout<<x.first<<":"<<x.second<<' ';
-            // }
-            // cout<<'\n';
             auto x = mp.upper_bound(minm);
-            // if(x == mp.end()){
-            //     if(--mp[nums[i+1]] == 0)
-            //         mp.erase(nums[i+1]);
-            //     minm = min(minm, nums[i]);
-            //     continue;
-            // }
-            // cout<<"("<<minm<<","<<
             if(x != mp.end() && minm < nums[i] && (*x).first < nums[i])
                 return true;
             if(--mp[nums[i+1]] == 0)
