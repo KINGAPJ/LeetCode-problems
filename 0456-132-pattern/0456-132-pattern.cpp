3794b7e3b7fa5338fac2c9e6b5cb4a0c71d4ba54
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-10-01 18:50:20 +0530

    Time: 192 ms (5.24%), Space: 64.4 MB (6.73%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        int s3 = INT_MIN;
-        stack<int> st;
-        for( int i = nums.size()-1; i >= 0; i -- ){
-            if( nums[i] < s3 ) return true;
-            else while( !st.empty() && nums[i] > st.top() ){ 
-              s3 = st.top(); st.pop(); 
-            }
-            st.push(nums[i]);
+        map<int, int> mp;
+        int n = nums.size();
+        int minm = nums[0];
+        for(int i = 2; i < n; i++){
+            mp[nums[i]]++;
+        }
+        // cout<<'\n';
+        for(int i = 1; i < n-1; i++){
+            // for(auto &x: mp){
+            //     cout<<x.first<<":"<<x.second<<' ';
+            // }
+            // cout<<'\n';
+            auto x = mp.upper_bound(minm);
+            // if(x == mp.end()){
+            //     if(--mp[nums[i+1]] == 0)
+            //         mp.erase(nums[i+1]);
+            //     minm = min(minm, nums[i]);
+            //     continue;
+            // }
+            // cout<<"("<<minm<<","<<
+            if(x != mp.end() && minm < nums[i] && (*x).first < nums[i])
+                return true;
+            if(--mp[nums[i+1]] == 0)
+                mp.erase(nums[i+1]);
+            minm = min(minm, nums[i]);
         }
         return false;
     }
