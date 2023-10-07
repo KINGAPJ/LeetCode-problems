d24c7bd6316a289e10df71a83ba5134c1e2c42ff
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-10-07 10:52:25 +0530

    Time: 166 ms (27.19%), Space: 10.8 MB (14.75%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> memo;
-    int MOD = 1e9 + 7;
-    int n;
-    int m;
-    
     int numOfArrays(int n, int m, int k) {
-        memo = vector(n, vector(m + 1, vector(k + 1, -1)));
-        this->n = n;
-        this->m = m;
-        return dp(0, 0, k);
-    }
-    
-    int dp(int i, int maxSoFar, int remain) {
-        if (i == n) {
-            if (remain == 0) {
-                return 1;
+        vector<vector<vector<int>>> dp(n + 1, vector(m + 1, vector(k + 1, 0)));
+        int MOD = 1e9 + 7;
+        
+        for (int num = 0; num < dp[0].size(); num++) {
+            dp[n][num][0] = 1;
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int maxSoFar = m; maxSoFar >= 0; maxSoFar--) {
+                for (int remain = 0; remain <= k; remain++) {
+                    int ans = 0;
+                    for (int num = 1; num <= maxSoFar; num++) {
+                        ans = (ans + dp[i + 1][maxSoFar][remain]) % MOD;
+                    }
+                    
+                    if (remain > 0) {
+                        for (int num = maxSoFar + 1; num <= m; num++) {
+                            ans = (ans + dp[i + 1][num][remain - 1]) % MOD;
+                        }
+                    }
+                    
+                    dp[i][maxSoFar][remain] = ans;
+                }
             }
-            
-            return 0;
-        }
-        
-        if (remain < 0) {
-            return 0;
-        }
-        
-        if (memo[i][maxSoFar][remain] != -1) {
-            return memo[i][maxSoFar][remain];
-        }
-        
-        int ans = 0;
-        for (int num = 1; num <= maxSoFar; num++) {
-            ans = (ans + dp(i + 1, maxSoFar, remain)) % MOD;
         }
 
-        for (int num = maxSoFar + 1; num <= m; num++) {
-            ans = (ans + dp(i + 1, num, remain - 1)) % MOD;
-        }
-        
-        memo[i][maxSoFar][remain] = ans;
-        return ans;
+        return dp[0][0][k];
     }
 };
