f784cb97540bb57a38c20022891303e21fb3fa25
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-10-07 11:03:33 +0530

    Time: 3 ms (98.62%), Space: 8.6 MB (41.94%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int numOfArrays(int n, int m, int k) {
-        vector<vector<vector<int>>> dp(n + 1, vector(m + 1, vector(k + 1, 0)));
+        long long dp[n + 1][m + 1][k + 1];
+        long long prefix[n + 1][m + 1][k + 1];
+        memset(dp, 0, sizeof(dp));
+        memset(prefix, 0, sizeof(prefix));
         int MOD = 1e9 + 7;
         
-        for (int num = 0; num < dp[0].size(); num++) {
-            dp[n][num][0] = 1;
+        for (int num = 1; num <= m; num++) {
+            dp[1][num][1] = 1;
+            prefix[1][num][1] = prefix[1][num - 1][1] + 1;
         }
         
-        for (int i = n - 1; i >= 0; i--) {
-            for (int maxSoFar = m; maxSoFar >= 0; maxSoFar--) {
-                for (int remain = 0; remain <= k; remain++) {
-                    int ans = 0;
-                    for (int num = 1; num <= maxSoFar; num++) {
-                        ans = (ans + dp[i + 1][maxSoFar][remain]) % MOD;
-                    }
+        for (int i = 1; i <= n; i++) {
+            for (int maxNum = 1; maxNum <= m; maxNum++) {
+                for (int cost = 1; cost <= k; cost++) {
+                    long long ans = (maxNum * dp[i - 1][maxNum][cost]) % MOD;
+                    ans = (ans + prefix[i - 1][maxNum - 1][cost - 1]) % MOD;
+
+                    dp[i][maxNum][cost] += ans;
+                    dp[i][maxNum][cost] %= MOD;
                     
-                    if (remain > 0) {
-                        for (int num = maxSoFar + 1; num <= m; num++) {
-                            ans = (ans + dp[i + 1][num][remain - 1]) % MOD;
-                        }
-                    }
-                    
-                    dp[i][maxSoFar][remain] = ans;
+                    prefix[i][maxNum][cost] = (prefix[i][maxNum - 1][cost] + dp[i][maxNum][cost]);
+                    prefix[i][maxNum][cost] %= MOD;
                 }
             }
         }
-
-        return dp[0][0][k];
+        
+        return prefix[n][m][k];
     }
 };
