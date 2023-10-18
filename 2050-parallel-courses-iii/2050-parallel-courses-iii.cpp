d43aab8e1fd55422f3a5fa2b166447a5159dd002
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-10-18 19:37:05 +0530

    Time: 337 ms (56.29%), Space: 131.9 MB (73.92%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
@@ -7,10 +7,6 @@ public:
             adj[i[0]-1].push_back(i[1]-1);
             inDeg[i[1]-1]++;
         }
-        // for(int i = 0; i < n; i++){
-        //     cout<<inDeg[i]<<' ';
-        // }
-        // cout<<'\n';
         queue<int> q;
         for(int i = 0; i < n; i++){
             if(inDeg[i] == 0)
@@ -22,16 +18,11 @@ public:
             ans = max(ans, starts[u]+time[u]);
             for(auto &i: adj[u]){
                 starts[i] = max(starts[i], starts[u]+time[u]);
-                ans = max(ans, starts[i]+time[i]);
                 if(--inDeg[i] == 0){
                     q.push(i);
                 }
             }
         }
-        // for(int i = 0; i < n; i++){
-        //     cout<<starts[i]<<' ';
-        // }
-        // cout<<'\n';
         return ans;
     }
 };
