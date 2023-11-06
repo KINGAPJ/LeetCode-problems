f42e953f443eb9e236d00c51f3fb21252a12e2c7
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-11-06 12:16:42 +0530

    Time: 80 ms (84.40%), Space: 63.4 MB (56.88%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
-        int i = 0, n = arr.size(), currInd = 0, wins = 0, maxElem = arr[0];
-        while(i < n){
+        int n = arr.size(), currInd = 0, wins = 0, maxElem = arr[0];
+        for(int i = 0; i < n; i++){
             if(i == currInd)
                 i++;
             if(i < n){
@@ -16,7 +16,6 @@ public:
                 if(wins == k)
                     return arr[currInd];
             }
-            i++;
         }
         return maxElem;
     }
