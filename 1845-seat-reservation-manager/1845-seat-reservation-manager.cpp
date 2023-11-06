5c5c5d20da507be692df9ee2e32d7928d37e2d64
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-11-06 12:40:18 +0530

    Time: 257 ms (97.56%), Space: 141.9 MB (96.99%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
@@ -1,23 +1,33 @@
 class SeatManager {
-    set<int> seats;
+    // Marker to point to unreserved seats.
+    int marker;
+
+    // Min heap to store all unreserved seats.
+    priority_queue<int, vector<int>, greater<int>> availableSeats;
+
 public:
     SeatManager(int n) {
-        for(int i = 1; i <= n; i++)
-            seats.insert(i);
+        // Set marker to the first unreserved seat.
+        marker = 1;
     }
+
     int reserve() {
-        int seat = *seats.begin();
-        seats.erase(seat);
-        return seat;
+        // If min-heap has any element in it, then,
+        // get the smallest-numbered unreserved seat from the min heap.
+        if (!availableSeats.empty()) {
+            int seatNumber = availableSeats.top();
+            availableSeats.pop();
+            return seatNumber;
+        }
+
+        // Otherwise, the marker points to the smallest-numbered seat.
+        int seatNumber = marker;
+        marker++;
+        return seatNumber;
     }
+
     void unreserve(int seatNumber) {
-        seats.insert(seatNumber);
+        // Push unreserved seat in the min heap.
+        availableSeats.push(seatNumber);
     }
 };
-
-/**
- * Your SeatManager object will be instantiated and called as such:
- * SeatManager* obj = new SeatManager(n);
- * int param_1 = obj->reserve();
- * obj->unreserve(seatNumber);
- */
