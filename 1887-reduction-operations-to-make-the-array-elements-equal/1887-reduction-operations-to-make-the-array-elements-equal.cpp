4bf32abe982e4041cb78d131944aadcc60aa21ec
Aditya Pratap Jena <76057491+KINGAPJ@users.noreply.github.com>
2023-11-19 13:47:52 +0530

    Time: 162 ms (61.35%), Space: 83.2 MB (68.40%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
-        map<int, int> frq;
-        int n = nums.size();
-        for(int i = 0; i < n; i++)
-            frq[nums[i]]++;
-        frq.erase(frq.begin());
-        int pref = 0, ans = 0;
-        for(map<int, int>::reverse_iterator i = frq.rbegin(); i != frq.rend(); i++){
-            pref += i->second;
-            ans += pref;
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int up = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] != nums[i - 1])
+                up++;            
+            ans += up;
         }
         return ans;
     }
